992dc5beebe94cf3d3bfe939e96ee1eb304cb66c
Nicolò <96006884+NikConte@users.noreply.github.com>
2024-07-18 12:15:23 +0200

    changed file path

--- PuzzleTowerDefense/MainMenuButtons.gd
+++ PuzzleTowerDefense/MainMenuButtons.gd
@@ -1,6 +1,6 @@
 extends ItemList
 
-var default_level = preload("res://pappaperol/lv_1.tscn")
+var default_level = preload("res://lv_1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +12,7 @@ func _process(delta):
 	
 	#start playing
 	if is_selected(0):
-		get_tree().change_scene_to_file("res://pappaperol/lv_1.tscn")
+		get_tree().change_scene_to_file("res://lv_1.tscn")
 	
 	#quit game
 	if is_selected(3):

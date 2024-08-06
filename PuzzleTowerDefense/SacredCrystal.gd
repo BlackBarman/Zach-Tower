73c4b8d026a286383fdeebacdbfe19e85a07ebba
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-06 11:31:14 +0200

    fixed name group
    Riccardo implemented a naming convention for groups, i'll push this change to make sure this keeps working when we merge our branches together

--- PuzzleTowerDefense/SacredCrystal.gd
+++ PuzzleTowerDefense/SacredCrystal.gd
@@ -4,7 +4,7 @@ class_name SacredCrystal
 @export var lose_screen : PackedScene
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("Enemy"):
+	if body.is_in_group("EnemyGroup"):
 		var x = lose_screen.instantiate()
 		get_tree().root.add_child(x)
 

08c515e4f26053077c15ef57200cc6e4be338ae0
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-06 11:19:13 +0200

    Basic fail state implemented
    a very very basic UI prompt that get's generated when an enemy touches it, our first fail state
    
    [ADDED]
    bunch of fonts
    UI theme
    class and scene for death popup

--- PuzzleTowerDefense/SacredCrystal.gd
+++ PuzzleTowerDefense/SacredCrystal.gd
@@ -3,9 +3,8 @@ class_name SacredCrystal
 
 @export var lose_screen : PackedScene
 
-
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Enemy"):
 		var x = lose_screen.instantiate()
 		get_tree().root.add_child(x)
-		print("ciao")
+

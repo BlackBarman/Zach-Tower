f136f03287954cc3f3d6630e0ec3cfaaf324ac96
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-22 15:46:49 +0200

    Revert "implemented win screen with level stat variables"
    This reverts commit ac5475fec6be6087cfc0ce5f31f03cada5e0e08f.

--- PuzzleTowerDefense/Level_Manager.gd
+++ PuzzleTowerDefense/Level_Manager.gd
@@ -8,8 +8,9 @@ extends Node
 # spawn rate in seconds
 @export var m_spawnRatio = 1.0
 
-@export var path : Path2D
-
+func _SetSpawnPath(a_SpawnPath):
+	$SpawnPath.curve = a_SpawnPath.curve
+	pass
 
 func _on_enemy_timer_timeout():
 	
@@ -21,9 +22,9 @@ func _on_enemy_timer_timeout():
 
 	# Choose a random location on the SpawnPath.
 	# We store the reference to the SpawnLocation node.
-	#var mob_spawn_location = path.cure
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
 	# And give it a random offset.
-	#mob_spawn_location.progress_ratio = randf()
+	mob_spawn_location.progress_ratio = randf()
 
 	#commented out as they are not doing anything,select and press ctrl+k to uncomment [start]
 	#var player_position = $SpawnPath/SpawnLocation.position
@@ -31,7 +32,7 @@ func _on_enemy_timer_timeout():
 	#commented out as they are not doing anything select and press ctrl+k to uncomment [end]
 	
 	# Spawn the mob by adding it to the Main scene.
-	path.add_child(mob)
+	$SpawnPath.add_child(mob)
 	m_numberEnemies = m_numberEnemies - 1
 
 
--- PuzzleTowerDefense/lv_1.gd
+++ PuzzleTowerDefense/lv_1.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#$TileMap/Path2D/LevelManager._SetSpawnPath($TileMap/Path2D)
 
-
-
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$TileMap/Path2D/LevelManager._SetSpawnPath($TileMap/Path2D)

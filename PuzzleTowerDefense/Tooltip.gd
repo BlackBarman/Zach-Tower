24c9b9440ead7bb0ad4f8565b490195cf60bc71a
Nicolò <96006884+NikConte@users.noreply.github.com>
2024-08-27 10:51:19 +0200

    Revert "Merge branch 'LinkTooltipToTower'"
    This reverts commit e9b92d667052eb8fd55406b96707b569e223f418, reversing
    changes made to 1ac21eeb6f34b90a359d347d70208e7e0f33c85f.

--- PuzzleTowerDefense/Tooltip.gd
+++ PuzzleTowerDefense/Tooltip.gd
@@ -3,27 +3,11 @@ class_name Tooltip
 
 @export var data : CustomData 
 
-var popup
-var DisplayPopup = false
-var hovered :bool = false
-
-signal buttonsSignal
 
 func _on_mouse_entered():
-	hovered = true
+	if data != null:
+		Popups.open_popup(Rect2i(Vector2i(global_position),Vector2i(size)),data)
 
 
 func _on_mouse_exited():
-	hovered = false
-
-
-
-func _input(event):
-	if hovered and DisplayPopup and event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and data != null:
-		Popups.open_popup(Rect2i(Vector2i(global_position),Vector2i(size)),data, get_parent() as BaseTower)
-
-
-func _on_base_tower_active_tooltip():
-	await get_tree().create_timer(0.5).timeout
-	DisplayPopup = true
-	pass # Replace with function body.
+	Popups.close_popup()

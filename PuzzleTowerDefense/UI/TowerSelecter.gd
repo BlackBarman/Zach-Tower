dd0f1168a628724923a992afbfcc65aa01f94a06
ricklusuardi <lusuardiriccardo@gmail.com>
2024-09-03 16:17:30 +0200

    fixed up tower selectern added new look for start battle button

--- PuzzleTowerDefense/UI/TowerSelecter.gd
+++ PuzzleTowerDefense/UI/TowerSelecter.gd
@@ -2,7 +2,7 @@ extends HBoxContainer
 class_name TowerSelecter
 
 @export var torri : Array[PackedScene] 
-@export var towers : Array[CustomData]
+#@export var towers : Array[CustomData]
 @export var tower_slot : PackedScene
 @onready var turnManager = get_tree().get_nodes_in_group("TurnManagerGroup")[0]
 signal set_tower
@@ -14,15 +14,15 @@ func _ready():
 
 func _set_tower_slots():
 	var index = 0
-	for i in towers:
+	for i in torri:
 		var towerSlot = tower_slot.instantiate()
 		add_child(towerSlot)
 		towerSlot.index = index
-		#var tempTower = i.instantiate()
-		towerSlot._set_preview_image(i.previewImage)
-		#tempTower.queue_free()
+		var tempTower = i.instantiate()
+		towerSlot._set_preview_image(tempTower.previewImage)
+		tempTower.queue_free()
 		index += 1
 
 func _set_tower(index : int):
 	var tower = torri[index]
-	emit_signal("set_tower", towers[index])
+	emit_signal("set_tower", tower)

69f3c706f5888cf24106684c2350a273178a4487
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-09-10 16:20:39 +0200

    commented ened level stats

--- PuzzleTowerDefense/Custom_Resources/End_Level_Stats.gd
+++ PuzzleTowerDefense/Custom_Resources/End_Level_Stats.gd
@@ -1,14 +1,14 @@
 extends Node
 class_name LevelStats
 
- 
-var shots_fired = 0
-var space_used = 0
-var money_spent = 0
-var overkill = 0
-var avg_tower_damage = 0 #TODO: implement set get function, i waan the ui to pull data dynamically
-var towers_placed = 0
-var inactive_towers = 0 
+
+var shots_fired = 0      #
+var space_used = 0       # TODO space used = number of towers placed (for now)
+var money_spent = 0      # TODO implement money variable in towers and update this variable each time a ower is placed
+var overkill = 0         # Nico TODO Implement a way to know how much damage is wasted when a given bullet hits a given enemy, then update this variable (in this script) accordingly
+var avg_tower_damage = 0 # Nico TODO: implement set get function, i want the ui to pull data dynamically
+var towers_placed = 0    # TODO: space used = number of towers placed (for now)
+var inactive_towers = 0  # Nico TODO: A way to know the towers that didn't get used
 
 
 func reset_data():
@@ -18,4 +18,4 @@ func reset_data():
 	overkill = 0
 	avg_tower_damage = 0 #TODO: implement set get function, i waan the ui to pull data dynamically
 	towers_placed = 0
-	inactive_towers = 0 
+	inactive_towers = 0

c6692055c57a6412fb6f1249c287007e288ef08e
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-10-31 10:27:24 +0100

    FIXED logic error in previus fix
    also commented and formatted the script

--- PuzzleTowerDefense/Levels/Level_Manager.gd
+++ PuzzleTowerDefense/Levels/Level_Manager.gd
@@ -9,10 +9,14 @@ var m_numberEnemies = 0
 @export var waves : Array[Wave] = []
 var path : Path2D
 var CurrentWave = 0
+var enemies_to_spawn
+
 
 func _ready():
 	for wave in waves:
 		m_numberEnemies += wave.m_numberEnemies
+	if waves.size() > 0:
+		enemies_to_spawn = waves[CurrentWave].m_numberEnemies
 
 
 func _turn_Start(TurnCounter: int):
@@ -24,28 +28,26 @@ func _turn_Start(TurnCounter: int):
 
 	# Itera su ogni wave
 	# Controlla se il turno corrente corrisponde al turno di spawn della wave
-	if waves[CurrentWave].m_numberEnemies > 0:
+	if  enemies_to_spawn > 0:
 		#print("Wave:",CurrentWave)
 		# Spawnare nemici per questa wave
 		for i in range(SpawnSize):
 			# Crea una nuova istanza della scena nemica
 			var mob = waves[CurrentWave].m_enemyScene.instantiate()
-			# Scegli una posizione casuale sul percorso di spawn
-			#var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
-			#mob_spawn_location.progress_ratio = randf()
 
 			# Recupera il nodo Path2D dal NodePath esportato in Wave
 			path = get_node(waves[CurrentWave].path)
 			if path and path is Path2D:
 				path.add_child(mob)
-				#waves[CurrentWave].m_numberEnemies -= 1
+				enemies_to_spawn = enemies_to_spawn - SpawnSize
+
 				m_numberEnemies = m_numberEnemies - SpawnSize
-				#print("mob spawned")
 			# Verifica se ci sono altri nemici da spawnare
 			if (m_numberEnemies <= 0):
 				break
 	else:
 		CurrentWave += 1
+		enemies_to_spawn = waves[CurrentWave].m_numberEnemies
 
 	if (m_numberEnemies < 0):
 		m_numberEnemies = 0
